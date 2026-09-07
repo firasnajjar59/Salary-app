14016f21a9f09a3490f05238979f91a996f74b8e
firas najjar <107777237+firasnajjar59@users.noreply.github.com>
2026-09-07 23:48:19 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -31,7 +31,7 @@ messaging.onBackgroundMessage((payload) => {
   });
 });
 
-const CACHE_NAME = 'salary-app-v74';
+const CACHE_NAME = 'salary-app-v75';
 const APP_FILES = [
   './',
   './index.html',
