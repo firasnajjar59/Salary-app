aca613532a6f765ea914a9baf2792666b28c21cd
firas najjar <107777237+firasnajjar59@users.noreply.github.com>
2026-09-09 16:23:46 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -31,7 +31,7 @@ messaging.onBackgroundMessage((payload) => {
   });
 });
 
-const CACHE_NAME = 'notebook-v1.0';
+const CACHE_NAME = 'notebook-v1.1';
 const APP_FILES = [
   './',
   './index.html',
